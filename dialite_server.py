c950a63c6988757ce1e88f6a8a2cc9ce42fef1af
aamodkh <khatiwadaaamod@gmail.com>
2023-01-14 04:53:10 +0545

    update notebooks

--- dialite_server.py
+++ dialite_server.py
@@ -3,37 +3,35 @@ import pandas as pd
 import discover
 import time
 
-def QuerySANTOS(query_table, intent_column, k):
-    #connect to SANTOS and return top-k unionable tables.
-    #for now we will use the dummy tables
-    if k == 1:
-        result = "Result"
-    else:
-        result = "Results"
-    time.sleep(4)
-    print("SANTOS top-"+str(k)+" "+ result +" added to the integration set.")
+def upload_query_table(filelocation):
+    file_name=filelocation.split("/")[-1]
+    print("Query table name:", file_name)
+    try:
+        query_table = pd.read_csv(filelocation, encoding="latin-1", on_bad_lines="skip")
+        query_table.head(5)
+        print("Query table uploaded successfully.")
+        return query_table
+    except:
+        print("Could not read the table.")
+        return 0
 
-def QueryJOSIE(query_table, intent_column, k):
-    #connect to JOSIE and return top-k unionable tables.
-    #for now we will use the dummy tables
-    if k == 1:
-        result = "Result"
-    else:
-        result = "Results"
-    time.sleep(4)
-    print("JOSIE top-"+str(k)+" "+ result+" added to the integration set.")
+def discover_tables(query_table, algorithm, k):
+    integration_set = set()
+    if "SANTOS" in algorithm:
+        print("Enter the value of k for SANTOS:")
+        k = int(input())
+        print("Enter index of intent column:")
+        intent_column = int(input())
+        integration_set = QuerySANTOS(query_table, intent_column, k)
 
-def FindIntegrationIDs():
-    #connect to ALIGN part of ALITE and assign dummy headers.
-    time.sleep(4)
-    print("Alignment task completed.")
+    if "JOSIE" in selected_algorithms:
+        print("Enter the value of k for JOSIE:")
+        k = int(input())
+        print("Enter index of query column:")
+        query_column = int(input())
+        dialite.QueryJOSIE(query_table, query_column, k)
 
-def ApplyALITEIntegration():
-    #connect to ALIGN part of ALITE and assign dummy headers.
-    time.sleep(4)
-    print("Integrated tables using ALITE.")
-
-def ApplyOuterJoinIntegration():
-    #connect to ALIGN part of ALITE and assign dummy headers.
-    time.sleep(4)
-    print("Integrated tables using Outer join.")
+def randomly_generate_query_table(prompt):
+    time.sleep(10)
+    table = pd.read_csv("gpt_table.csv")
+    return table
