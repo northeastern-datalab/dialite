8e37da3bad742ee7ad83c598190c01c7fd9ef98f
aamodkh <khatiwadaaamod@gmail.com>
2023-01-16 07:37:58 +0545

    Update notebook

--- dialite_server.py
+++ dialite_server.py
@@ -36,7 +36,10 @@ def query_josie(query_table, query_column, k):
 
 def integrate_alite(integration_set):
     #return pre-integrated table for initial demo.
-    integrated_table_location = "data/integration-result/alite_fd_covid19_t1.csv"
+    if "stadiums" in list(integration_set)[0]: #.contains("stadiums"):
+        integrated_table_location = "data/integration-result/alite_fd_stadiums_0.csv"
+    else:
+        integrated_table_location = "data/integration-result/alite_fd_covid19_t1.csv"
     integrated_table = pd.read_csv(integrated_table_location, encoding="latin-1", on_bad_lines="skip")
     return integrated_table
 
@@ -62,19 +65,23 @@ def discover_tables(query_table, algorithm, k):
 
 def integrate_tables(integration_set, algorithm):
     if algorithm == "ALITE":
-        query_table_name = list(integration_set)[0].rsplit("/",2)[-1]
+        #query_table_name = list(integration_set)[0].rsplit("/",2)[-1]
         integrated_table = integrate_alite(integration_set)
         #integrated_table_location = "data/integration-result/alite_fd_"+query_table_name
         #integrated_table.to_csv(integrated_table_location, index=False)
         print("Successfully integrated "+ str(len(integration_set)) + " tables using ALITE.")
         print("Integrated table:")
         print(integrated_table)
+    
+    if algorithm == "outer_join":
+        #for initial submission, we return a pre-integrated table without calling the user defined function.
+        integrated_table_location = "data/integration-result/outerjoin_stadiums_0.csv"
+        integrated_table = pd.read_csv(integrated_table_location, encoding="latin-1", on_bad_lines="skip")
+        print("Successfully integrated "+ str(len(integration_set)) + " tables using outer join algorithm.")
+        print("Integrated table:")
+        print(integrated_table)
     return integrated_table
 
-def randomly_generate_query_table(prompt):
-    time.sleep(10)
-    table = pd.read_csv("gpt_table.csv")
-    return table
 
 def get_table_name(table):
     table = table.rsplit("/",1)[-1]
@@ -84,3 +91,37 @@ def analyze_sql(integrated_table, query):
     # We execute the query manually 
     integrated_table.dropna(subset = ['Vaccination Rate'], inplace = True)
     print(ps.sqldf(query, locals()))
+
+
+def randomly_generate_query_table(prompt):
+    # We have not show GPT connection to keep the query table deterministic for the demo.
+    time.sleep(5)
+    table = pd.read_csv("data/query/gpt_table_1.csv")
+    return table
+
+def new_joinability_discovery_algorithm(df1, df2):
+    join_df = pd.merge(df1, df2, how ='inner')
+    return len(join_df)/max(len(df1), len(df2))
+
+def new_outer_join_integration_algorithm(integration_set):
+    table1_loc = integration_set.pop()
+    table1 = pd.read_csv(table1_loc)
+    for table2_loc in integration_set:
+            table2 = pd.read_csv(table2_loc)
+            table1 = table1.merge(table2, how = "outer")
+    return table1
+
+
+def analyze_er(integrated_table):
+    # We use py entitymatching package to run the entity resolution algorithm.
+    # Currently, we have not integrated the API. So we show the results of applying er using py entitymatching
+    # manually. These number are also reported in the full ALITE paper.
+    # The related notebook is available in this github repo with name: analyze_entity_resolution.ipynb
+    if integrated_table.shape[0] > 115 :
+        print("Precision: 0.795")
+        print("Recall: 0.838")
+        print("F-score: 0.816")
+    else:
+        print("Precision: 0.339")
+        print("Recall: 0.397")
+        print("F-score: 0.366")
