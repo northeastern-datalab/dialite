4e8c2367248355fed025a43430bb372c65902457
aamodkh <khatiwadaaamod@gmail.com>
2023-03-28 23:10:45 -0400

    Update data generation

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import openai
 app = Flask(__name__)
 
 def QueryGPT3(prompt):
-    openai.api_key = "OPENAI_API_KEY"
+    #openai.api_key = "OPENAI_API_KEY"
+    openai.api_key = "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"
     response = openai.Completion.create(
     model="text-davinci-003",
     prompt=prompt,
