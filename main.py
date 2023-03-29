af4abb945bd14592d4ea0f7d13a03441b0151ae6
aamodkh <khatiwadaaamod@gmail.com>
2023-03-28 22:57:48 -0400

    Add GPT3 Generation

--- main.py
+++ main.py
@@ -2,9 +2,38 @@ from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import csv
 import os
+import openai
 
 app = Flask(__name__)
 
+def QueryGPT3(prompt):
+    openai.api_key = "OPENAI_API_KEY"
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    return response.choices[0]['text']
+
+def ConvertTextToTable(text):
+    # Splitting the string by newline character
+    rows = text.split('\n')
+    # Extracting column names from the first row
+    columns = [col.strip() for col in rows[1].split('|') if col.strip()]
+    # Extracting the data from the rest of the rows
+    data_rows = []
+    for row in rows[2:]:
+        data_rows.append([col.strip() for col in row.split('|') if col.strip()])
+    columns = data_rows.pop(0)
+    separator = data_rows.pop(0)
+    # Creating pandas DataFrame
+    df = pd.DataFrame(data_rows, columns=columns)
+    # print the resulting DataFrame
+    return df
 
 @app.route("/")
 def index():    
@@ -35,6 +64,25 @@ def upload_table():
             return jsonify({'success': False, 'message': 'No file to upload!'})
     except Exception as e:
         return jsonify({'success': False, 'message': str(e)})    
-    
+
+@app.route("/generate_query", methods=["POST"])
+def generate_table():
+    try:
+        #"generate a table about covid with 5 columns and 10 rows"
+        text_prompt = request.form['query_prompt'] 
+        generated_query_name = request.form['generated_query_name']
+        table_path = "data"+os.sep+"query"+os.sep+generated_query_name+".csv"
+        if os.path.isfile(table_path) == True:
+                return jsonify({'success': False, 'message': 'File already exists. Please give a different name!'})
+        table_text = QueryGPT3(text_prompt)
+        table = ConvertTextToTable(table_text)
+        table.to_csv(table_path, index=False)
+        table_sample = table.head(5)
+        message =  "Query Table named "+ generated_query_name +".csv with "
+        message += str(table.shape[0])+" rows and "+ str(table.shape[1]) +" columns "
+        message += "Generated successfully! See Sample table."           
+        return jsonify({'success': True, 'message': message, 'table': table_sample.to_html()})
+    except Exception as e:
+        return jsonify({'success': False, 'message': str(e), 'table':"Table not generated"})
 if __name__ == "__main__":
     app.run(debug=True)
